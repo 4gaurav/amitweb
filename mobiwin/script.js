2ae718df92ec588d69ec05769954c93d07d8bf46
4gaurav <107033627+4gaurav@users.noreply.github.com>
2024-02-12 09:14:23 +0530

    Add files via upload

---
+++ mobiwin/script.js
@@ -0,0 +1,30 @@
+
+
+  
+  var swiper = new Swiper('.mySwiper-2', {
+    centeredSlides: true,
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
+  
+  var swiper = new Swiper('.mySwiper', {
+    centeredSlides: true,
+    slidesPerView: 2,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
